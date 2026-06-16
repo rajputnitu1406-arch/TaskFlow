8a1183692abd28bf5e093630b11d3b235ec88f48
Nitu Rajput <rajputnitu1406@gmail.com>
2026-06-16 14:21:25 +0530

    minor changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://taskflow-132z.onrender.com/',
+  baseURL: 'https://taskflow-132z.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
